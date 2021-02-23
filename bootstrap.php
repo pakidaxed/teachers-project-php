c093fa731eaa9ff38ac6d0e364a0bd49a934e52d
Tomas Jucius <jucius.tomas@gmail.com>
2021-02-23 17:30:05 +0200

    docker fully added php7.4 nginx.latest mysql5.7 nginx config file

--- bootstrap.php
+++ bootstrap.php
@@ -3,7 +3,7 @@
 require  __DIR__ . '/vendor/autoload.php';
 
 $config = [
-    'db_dsn' => 'mysql:host=172.25.0.2;dbname=nfq_task',
+    'db_dsn' => 'mysql:host=nfq-mysql57;dbname=nfq_task',
     'db_user' => 'root',
     'db_pass' => 'secret'
 ];
